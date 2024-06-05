66fd5f9ba03814c700451378d0f825527e1dcae5
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2024-06-05 08:06:27 -0700

    Add custom patch support

--- src/audio/sound_bank.cpp
+++ src/audio/sound_bank.cpp
@@ -1,77 +1,81 @@
 #include <vector>
-#include "al/ui/al_ControlGUI.hpp"
+#include <string>
+#include <iostream>
+#include <filesystem>
 
 #include "pcm_env.cpp"
+
+namespace fs = std::filesystem;
 
 using namespace al;
 
 // 38 patches (0-37)
-std::vector<Patch*> SOUND_BANK = {
+std::vector<Patch *> SOUND_BANK = {
   new PatchListSeparator("FEATURED"),
   new Timbre(37, "Lo-Fi", "Lo-Fi Piano", {41, 46, 48, 57, 68}),
   new Timbre(31, "Essentials", "House Piano", {51, 63, 66, 70, 73}),
-  new Timbre(8, "Future Bass", "Summer Breeze", {61,65,68,72,75,80,84,87}, 12),
+  new Timbre(8, "Future Bass", "Summer Breeze", {61, 65, 68, 72, 75, 80, 84, 87}, 12),
   new Timbre(29, "Essentials", "Synthwave Chords", {51, 63, 66, 70, 73}, 12),
-  new Timbre(4, "Essentials", "Marimba", {77,79,84,89,91,96,100}),
-  new Timbre(10, "Essentials", "Reflection Guitar", {61,65,68,72}, 12),
+  new Timbre(4, "Essentials", "Marimba", {77, 79, 84, 89, 91, 96, 100}),
+  new Timbre(10, "Essentials", "Reflection Guitar", {61, 65, 68, 72}, 12),
   new Timbre(32, "Essentials", "Log Bass", {32, 39, 42, 44, 48, 49, 56, 58}, 12),
   new Timbre(30, "Essentials", "Wire Bass", {51, 63, 66, 70, 73}, 12),
   new Timbre(35, "Lo-Fi", "Electric Bass", {48, 60}),
   new Timbre(16, "Lo-Fi", "Dancing Strings", {65}),
 
   new DrumKit(33,
-    "Drum Kit",
+              "Drum Kit",
 
-    std::vector<std::string> {
-      "KICK-POP",
-      "KICK-DAC",
-      "KICK-SK",
-      
-      "SNARE-DAC",
-      "CLAP-POP",
+              std::vector<std::string>{
+                "KICK-POP",
+                "KICK-DAC",
+                "KICK-SK",
 
-      "HAT-DANCE",
-      "HAT-DAC",
-      "HAT-SK",
+                "SNARE-DAC",
+                "CLAP-POP",
 
-      "SLAP-POP",
-      "SLIDE-POP",
-      "SLIDE-POP-OFFSET",
-      
-      "CRASH-LO",
-      "CRASH-HI",
-      "CRASH-HI",
+                "HAT-DANCE",
+                "HAT-DAC",
+                "HAT-SK",
 
-      "SFX-BELL",
-      "SFX-BOWL-LO",
-      "SFX-BOWL-HI",
-      "SFX-BREATH",
-      "SFX-DROP-LO",
-      "SFX-DROP-HI",
-      "SFX-GOURD",
-      "SFX-SHAKE-OFFSET",
-      "SFX-SHAKE"
-    },
+                "SLAP-POP",
+                "SLIDE-POP",
+                "SLIDE-POP-OFFSET",
 
-    std::map<std::string, int> {
-      {"CRASH-LO", 3},
-      {"SFX-LOTUSBEND", -1},
-      {"SFX-MK939", 0}
-    }
+                "CRASH-LO",
+                "CRASH-HI",
+                "CRASH-HI",
+
+                "SFX-BELL",
+                "SFX-BOWL-LO",
+                "SFX-BOWL-HI",
+                "SFX-BREATH",
+                "SFX-DROP-LO",
+                "SFX-DROP-HI",
+                "SFX-GOURD",
+                "SFX-SHAKE-OFFSET",
+                "SFX-SHAKE"
+              },
+
+              std::map<std::string, int>{
+                {"CRASH-LO", 3},
+                {"SFX-LOTUSBEND", -1},
+                {"SFX-MK939", 0}
+              }
   ),
 
   new PatchListSeparator("FUTURE BASS PACK"),
-  new Timbre(1, "Future Bass", "Tokyo Lights", {84,88,96}),
-  new Timbre(6, "Future Bass", "Fresh!", {65,67,72,79,84,89,96,101,103,105,106,108}),
-  new Timbre(7, "Future Bass", "DECORATE", {87,89,91,92,94,96,99,101}),
-  new Timbre(2, "Future Bass", "Spring emotion", {72,77,84}),
-  new Timbre(3, "Future Bass", "Tempura", {62,65,69,72}),
-  new Timbre(9, "Future Bass", "Sequence Saw", {65,68,72}),
-  new Timbre(5, "Future Bass", "Square Synth", {60,65,67,69,72,77,79,84}),
-  new Timbre(11, "Future Bass", "Mirai!", {61,65,68,72,75,80,84,87}),
-  new Timbre(12, "Future Bass", "Summer Sweat", {61,65,68,72,75,80,84,87}, 12),
+  new Timbre(1, "Future Bass", "Tokyo Lights", {84, 88, 96}),
+  new Timbre(6, "Future Bass", "Fresh!", {65, 67, 72, 79, 84, 89, 96, 101, 103, 105, 106, 108}),
+  new Timbre(7, "Future Bass", "DECORATE", {87, 89, 91, 92, 94, 96, 99, 101}),
+  new Timbre(2, "Future Bass", "Spring emotion", {72, 77, 84}),
+  new Timbre(3, "Future Bass", "Tempura", {62, 65, 69, 72}),
+  new Timbre(9, "Future Bass", "Sequence Saw", {65, 68, 72}),
+  new Timbre(5, "Future Bass", "Square Synth", {60, 65, 67, 69, 72, 77, 79, 84}),
+  new Timbre(11, "Future Bass", "Mirai!", {61, 65, 68, 72, 75, 80, 84, 87}),
+  new Timbre(12, "Future Bass", "Summer Sweat", {61, 65, 68, 72, 75, 80, 84, 87}, 12),
   new Timbre(18, "Future Bass", "SK-Pizzicato", {36, 48, 60, 72}, -14),
-  new Timbre(13, "Future Bass", "Kokoponkon", {61,65,68,72,75,80,84,87}),
+  new Timbre(13, "Future Bass", "Kokoponkon", {61, 65, 68, 72, 75, 80, 84, 87}),
 
   new PatchListSeparator("LO-FI PACK"),
   new Timbre(14, "Lo-Fi", "YM-2800 DAC Piano", {41, 46, 48, 57, 68}),
@@ -92,13 +96,56 @@ std::vector<Patch*> SOUND_BANK = {
   // new Timbre(25, "Lo-Fi", "Sweet Dreams", {70, 85}),
 };
 
-std::map<int, Patch*> SOUND_BANK_MAP = {};
+std::string getFileNameWithoutExtension(const fs::path &filePath) {
+  return filePath.stem().string(); // stem() returns the filename without the extension
+}
 
-void makeSoundBankMap()
-{
-  for (int i = 0; i < SOUND_BANK.size(); i++)
-  {
-    Patch* patch = SOUND_BANK.at(i);
+int convertStringToInt(const std::string &str) {
+  try {
+    return std::stoi(str); // stoi() converts the string to an integer
+  } catch (const std::invalid_argument &e) {
+    std::cerr << "Invalid argument: " << e.what() << std::endl;
+    return -1; // Return an error code or handle it as needed
+  } catch (const std::out_of_range &e) {
+    std::cerr << "Out of range: " << e.what() << std::endl;
+    return -1; // Return an error code or handle it as needed
+  }
+}
+
+std::vector<int> processWavFiles(const fs::path &directoryPath) {
+  std::vector<int> pitches;
+  for (const auto &entry : fs::directory_iterator(directoryPath)) {
+    if (entry.is_regular_file() && entry.path().extension() == ".wav") {
+      std::string fileNameWithoutExtension = getFileNameWithoutExtension(entry.path());
+      if (int fileNameAsInt = convertStringToInt(fileNameWithoutExtension); fileNameAsInt != -1) {
+        std::cout << "Filename: " << fileNameWithoutExtension << ", as int: " << fileNameAsInt << std::endl;
+        pitches.push_back(fileNameAsInt);
+      }
+    }
+  }
+  return pitches;
+}
+
+void addCustomPatches() {
+  const fs::path path = "./timbre/Custom";
+  int i = 38;
+  for (const auto &entry : fs::directory_iterator(path)) {
+    if (entry.is_directory()) {
+      if (auto pitches = processWavFiles(entry.path()); !pitches.empty()) {
+        if (i == 38) { // Add a separator before the first custom patch
+          SOUND_BANK.push_back(new PatchListSeparator("CUSTOM"));
+        }
+        SOUND_BANK.push_back(new Timbre(i, "Custom", entry.path().filename().string(), pitches));
+        i++;
+      }
+    }
+  }
+}
+
+std::map<int, Patch *> SOUND_BANK_MAP = {};
+
+void makeSoundBankMap() {
+  for (auto patch : SOUND_BANK) {
     SOUND_BANK_MAP[patch->id] = patch;
   }
 }
